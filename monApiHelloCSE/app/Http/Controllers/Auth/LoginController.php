4bb0ee147120092e8397dc813327942f974ab47e
dw208 <dw2083290@gmail.com>
2025-05-22 12:46:57 +0200

    Installation de Laravel Sanctum ...
     configuration du model User et creation d'un endpoint de connexion Login (rajout d'une route login dans api.php)

---
+++ monApiHelloCSE/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    //
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $admin = User::where('email', $request->email)->first();
+
+        if (! $admin || ! Hash::check($request->password, $admin->password)) {
+            throw ValidationException::withMessages([
+                'email' => [__('auth.failed')],
+            ]);
+        }
+
+        $token = $admin->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+}
